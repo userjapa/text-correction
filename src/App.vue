9c1b56dafe4ce1b8f8ac9a4216328f878740833b
usuarioJapa <willian.hsimabukuro@gmail.com>
2018-09-03 15:15:01 -0300

    finished

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,36 @@
     <h2>Game</h2>
     <div class="game-tc">
       <div class="game-tc__media">
-        <audio :src="question.audio" controls/>
+        <audio ref="audio"
+               :src="question.audio"
+               @playing="playing = true"
+               @pause="playing = false"
+               @loadeddata="duration = $event.target.duration"
+               @timeupdate="time = $event.target.currentTime"/>
+        <div class="game-tc__media__player">
+          <span @click="playing?$refs['audio'].pause():$refs['audio'].play()">{{ playing?'Pause':'Play' }}</span>
+          <input type="range"
+                 step="0.00001"
+                 v-model="time"
+                 :max="duration"
+                 @input="$refs['audio'].currentTime = time">
+        </div>
       </div>
       <div class="game-tc__answer">
-        <input type="text" name="" v-model="question.answer">
+        <input type="text"
+               name="answer"
+               v-model="question.answer"
+               :class="{
+                 right: (question.answered && question.hit),
+                 wrong: (question.answered && !question.hit)
+               }">
       </div>
-      <div class="game-tc__options">
-        <button type="button" name="confirm" @click="confirm(question)" :disabled="!question.answer">Confirm</button>
+      <div class="game-tc__options" v-if="!ended">
+        <button type="button" name="confirm" @click="confirm(question)" :disabled="!question.answer" v-if="!question.answered">Confirm</button>
+        <button type="button" name="next" @click="setCurrent(questions)" v-else>Next</button>
+      </div>
+      <div class="game-tc__options" v-else>
+        <button type="button" name="finish">Finish</button>
       </div>
     </div>
   </div>
@@ -22,23 +45,47 @@ export default {
   name: 'app',
   data () {
     return {
-      question: {
+      questions: [{
         audio: 'https://vignette.wikia.nocookie.net/leagueoflegends/images/e/ef/Kayn.attack22.ogg/revision/latest?cb=20170918212301',
-        question: 'Found you',
+        question: 'Found, you!',
         answer: '',
         answered: false,
         hit: false
-      }
+      }],
+      question: {},
+      ended: false,
+      index: 0,
+      playing: false,
+      time: 0,
+      duration: 0
     }
   },
   methods: {
+    setCurrent (questions) {
+      let index = 0
+      let qst = {}
+      for (const ind in questions) {
+        index = ind
+        if (!questions[ind].answered) {
+          qst = questions[ind]
+          break
+        } else if (ind == (questions.length - 1)) {
+          qst = questions[ind]
+          this.ended = true
+        }
+      }
+      this.$set(this, 'question', qst)
+      this.$set(this, 'index', index)
+    },
     confirm (question) {
-      const text = question.question
-      const answer = question.answer
+      const text = question.question.replace(/[,.!?]/g, '').toLowerCase()
+      const answer = question.answer.replace(/[,.!?]/g, '').toLowerCase()
       this.$set(question, 'answered', true)
       this.$set(question, 'hit', text == answer)
-      console.log(question)
     }
+  },
+  mounted () {
+    this.setCurrent(this.questions)
   }
 }
 </script>
@@ -58,11 +105,34 @@ h1, h2 {
 }
 
 .game-tc {
-  &, & div {
-    border: 1px solid black;
-  }
   div:not(:last-child) {
     margin-bottom: 15px;
+  }
+  &__media {
+    display: flex;
+    justify-content: center;
+    &__player {
+      width: 300px;
+      height: 39px;
+      border: 2px solid gray;
+      border-radius: 25px;
+      display: flex;
+      span {
+        cursor: pointer;
+        display: inline-block;
+        border: 2px solid gray;
+        border-radius: 25px;
+        padding: 5px;
+        margin: 5px;
+        flex-basis: calc(20% - 20px);
+      }
+      input {
+        height: 100%;
+        margin: 0;
+        margin-right: 15px;
+        flex-basis: calc(80% - 15px);
+      }
+    }
   }
   &__answer {
     input {
@@ -73,10 +143,18 @@ h1, h2 {
       font-size: 16px;
       width: 270px;
       text-align: center;
+      &.right {
+        background-color: green;
+      }
+      &.wrong {
+        background-color: red;
+      }
     }
   }
   &__options {
     button {
+      font-size: 16px;
+      width: 300px;
       cursor: pointer;
       padding: 10px 30px;
       border: none;
